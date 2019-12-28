90f50780cceb99c5dd094f03a39a5dfdfeae738a
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-12-28 16:06:58 -0300

    type and scalar

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -1,9 +1,12 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
+    scalar Date
+
+    # retornar Date só é possível por conta da declaração scalar
     type Query {
         ola: String
-        horaAtual: String
+        horaAtual: Date
     }
 `;
 
@@ -13,7 +16,7 @@ const resolvers = {
             return 'E aí!'
         },
         horaAtual(){
-            return `A hora atual é: ${new Date()}`
+            return new Date
         }
     }
 };
@@ -26,3 +29,17 @@ const server = new ApolloServer({
 server.listen().then(({ url }) => {
   console.log(`Servidor sendo executado em: ${url}`); 
 });
+
+
+/**
+ * 
+ * Os tipos aceitos por padrão pelo GraphQL são:
+ * String
+ * Int
+ * Float
+ * Boolean
+ * e ID
+ * ....
+ * Ainda assim é possível declarar um tipo "customizado" para retornar uma classe
+ * utilizando o "type", ou para classes prontas da linguagem, como é o caso do Date, usamos o scalar
+ */
