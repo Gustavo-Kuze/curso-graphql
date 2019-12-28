5920aee67826e25fe0a2ffa13b478014ce3c91cc
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-12-28 18:26:17 -0300

    creating resolvers to custom types

--- schema-query/projeto/index.js
+++ schema-query/projeto/index.js
@@ -3,20 +3,45 @@ const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
     scalar Date
 
+    type Usuario {
+        id: ID
+        nome: String
+        email: String
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # retornar Date só é possível por conta da declaração scalar
     type Query {
         ola: String
         horaAtual: Date
+        usuarioLogado: Usuario
     }
 `;
 
 const resolvers = {
+    Usuario: {
+        salario(usuario){
+            return usuario.salario_real
+        }
+    },
     Query: {
         ola(){
             return 'E aí!'
         },
         horaAtual(){
             return new Date
+        },
+        usuarioLogado(){
+            return {
+                id: 1,
+                nome: 'Fulano da Silva',
+                email: 'ful.silvvva@mail.com',
+                idade: 23,
+                salario_real: 3233.23,
+                vip: true
+            }
         }
     }
 };
